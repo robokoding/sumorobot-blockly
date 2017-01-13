279366ca63879d5520d49cb129b247f893e7b3e5
Silver Kuusik <silver.kuusik@gmail.com>
2017-01-13 15:35:16 +0100

    Update en.js

--- msg/js/en.js
+++ msg/js/en.js
@@ -1,7 +1,7 @@
 /* sumorobot message translations */
 /* delay block */
 Blockly.Msg.SUMOROBOT_DELAY_TITLE = "delay";
-Blockly.Msg.SUMOROBOT_DELAY_HELPURL = "https://codesnail.koodur.com/robokoding/";
+Blockly.Msg.SUMOROBOT_DELAY_HELPURL = "http://www.robokoding.com/sumorobot/";
 Blockly.Msg.SUMOROBOT_DELAY_TOOLTIP = "Continue doing previouse movement for given milliseconds.";
 /* move block */
 Blockly.Msg.SUMOROBOT_MOVE_MSG_STOP = "move stop";
@@ -9,17 +9,17 @@ Blockly.Msg.SUMOROBOT_MOVE_MSG_LEFT = "move left";
 Blockly.Msg.SUMOROBOT_MOVE_MSG_RIGHT = "move right";
 Blockly.Msg.SUMOROBOT_MOVE_MSG_FORWARD = "move forward";
 Blockly.Msg.SUMOROBOT_MOVE_MSG_BACKWARD = "move backward";
-Blockly.Msg.SUMOROBOT_MOVE_HELPURL = "https://codesnail.koodur.com/robokoding/";
+Blockly.Msg.SUMOROBOT_MOVE_HELPURL = "http://www.robokoding.com/sumorobot/";
 Blockly.Msg.SUMOROBOT_MOVE_TOOLTIP = "Move the Sumorobot to a specific direction.";
 /* enemy block */
 Blockly.Msg.SUMOROBOT_ENEMY_MSG_LEFT = "enemy left";
 Blockly.Msg.SUMOROBOT_ENEMY_MSG_FRONT = "enemy front";
 Blockly.Msg.SUMOROBOT_ENEMY_MSG_RIGHT = "enemy right";
-Blockly.Msg.SUMOROBOT_ENEMY_HELPURL = "https://codesnail.koodur.com/robokoding/";
+Blockly.Msg.SUMOROBOT_ENEMY_HELPURL = "http://www.robokoding.com/sumorobot/";
 Blockly.Msg.SUMOROBOT_ENEMY_TOOLTIP = "Detect the enemy in the Sumoring.";
 /* line block */
 Blockly.Msg.SUMOROBOT_LINE_MSG_LEFT = "line left";
 Blockly.Msg.SUMOROBOT_LINE_MSG_RIGHT = "line right";
 Blockly.Msg.SUMOROBOT_LINE_MSG_FRONT = "line front";
-Blockly.Msg.SUMOROBOT_LINE_HELPURL = "https://codesnail.koodur.com/robokoding/";
+Blockly.Msg.SUMOROBOT_LINE_HELPURL = "http://www.robokoding.com/sumorobot/";
 Blockly.Msg.SUMOROBOT_LINE_TOOLTIP = "Detect the line of the Sumoring.";
