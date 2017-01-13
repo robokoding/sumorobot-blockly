5a4d9ad426c1bc449e04a6b0bf4291a79c7381ca
Silver Kuusik <silver.kuusik@gmail.com>
2017-01-13 15:35:43 +0100

    Update et.js

--- msg/js/et.js
+++ msg/js/et.js
@@ -1,23 +1,7 @@
 /* sumorobot message translations */
-/* if block */
-Blockly.Msg.CONTROLS_IF_ELSEIF_TOOLTIP = "Lisa kui plokile tingimus.";
-Blockly.Msg.CONTROLS_IF_ELSE_TOOLTIP = "Lisa kui plokile loplik, catch-all tingimus.";
-Blockly.Msg.CONTROLS_IF_HELPURL = "https://code.google.com/p/blockly/wiki/If_Then";
-Blockly.Msg.CONTROLS_IF_IF_TOOLTIP = "Kui ploki umber seadistamiseks, lisa, eemalda, voi sorteeri umber sektsioone.";
-Blockly.Msg.CONTROLS_IF_MSG_ELSE = "voi";
-Blockly.Msg.CONTROLS_IF_MSG_ELSEIF = "voi kui";
-Blockly.Msg.CONTROLS_IF_MSG_IF = "kui";
-Blockly.Msg.CONTROLS_IF_MSG_THEN = "siis";
-Blockly.Msg.CONTROLS_IF_TOOLTIP_1 = "Kui uks vaartus on tosi, siis taida kask.";
-Blockly.Msg.CONTROLS_IF_TOOLTIP_2 = "Kui uks vaartus on tosi, siis taida esimene plokk kaske.  Vastand juhul, taida teine plokk kaske.";
-Blockly.Msg.CONTROLS_IF_TOOLTIP_3 = "Kui esimene vaartus on tosi, siis taida esimene plokk kaske.  Vastand juhul, kui teine vaartus on tosi, taida teine plokk kaske.";
-Blockly.Msg.CONTROLS_IF_TOOLTIP_4 = "Kui esimene vaartus on tosi, siis taida esimene plokk kaske.  Vastasel juhul, kui teine vaartus on tosi, siis taida teine plokk kaske.  Kui ukski vaartustest pole tosi, taida viimane plokk kaske.";
-Blockly.Msg.CONTROLS_IF_IF_TITLE_IF = Blockly.Msg.CONTROLS_IF_MSG_IF;
-Blockly.Msg.CONTROLS_IF_ELSEIF_TITLE_ELSEIF = Blockly.Msg.CONTROLS_IF_MSG_ELSEIF;
-Blockly.Msg.CONTROLS_IF_ELSE_TITLE_ELSE = Blockly.Msg.CONTROLS_IF_MSG_ELSE;
 /* delay block */
 Blockly.Msg.SUMOROBOT_DELAY_TITLE = "oota";
-Blockly.Msg.SUMOROBOT_DELAY_HELPURL = "https://codesnail.koodur.com/robokoding/";
+Blockly.Msg.SUMOROBOT_DELAY_HELPURL = "http://www.robokoding.com/sumorobot/";
 Blockly.Msg.SUMOROBOT_DELAY_TOOLTIP = "Jatka liikumist eelnevalt maaratud suunda antud millisekunditeks.";
 /* move block */
 Blockly.Msg.SUMOROBOT_MOVE_MSG_STOP = "stopp";
@@ -25,17 +9,17 @@ Blockly.Msg.SUMOROBOT_MOVE_MSG_LEFT = "vasakule";
 Blockly.Msg.SUMOROBOT_MOVE_MSG_RIGHT = "paremale";
 Blockly.Msg.SUMOROBOT_MOVE_MSG_FORWARD = "edasi";
 Blockly.Msg.SUMOROBOT_MOVE_MSG_BACKWARD = "tagasi";
-Blockly.Msg.SUMOROBOT_MOVE_HELPURL = "https://codesnail.koodur.com/robokoding/";
+Blockly.Msg.SUMOROBOT_MOVE_HELPURL = "http://www.robokoding.com/sumorobot/";
 Blockly.Msg.SUMOROBOT_MOVE_TOOLTIP = "Soida Sumorobotiga maaratud suunda.";
 /* enemy block */
 Blockly.Msg.SUMOROBOT_ENEMY_MSG_LEFT = "vastane vasakul";
 Blockly.Msg.SUMOROBOT_ENEMY_MSG_FRONT = "vastane ees";
 Blockly.Msg.SUMOROBOT_ENEMY_MSG_RIGHT = "vastane parem";
-Blockly.Msg.SUMOROBOT_ENEMY_HELPURL = "https://codesnail.koodur.com/robokoding/";
+Blockly.Msg.SUMOROBOT_ENEMY_HELPURL = "http://www.robokoding.com/sumorobot/";
 Blockly.Msg.SUMOROBOT_ENEMY_TOOLTIP = "Tuvasta vastane Sumovaljakul.";
 /* line block */
 Blockly.Msg.SUMOROBOT_LINE_MSG_LEFT = "joon vasakul";
 Blockly.Msg.SUMOROBOT_LINE_MSG_RIGHT = "joon paremal";
 Blockly.Msg.SUMOROBOT_LINE_MSG_FRONT = "joon ees";
-Blockly.Msg.SUMOROBOT_LINE_HELPURL = "https://codesnail.koodur.com/robokoding/";
+Blockly.Msg.SUMOROBOT_LINE_HELPURL = "http://www.robokoding.com/sumorobot/";
 Blockly.Msg.SUMOROBOT_LINE_TOOLTIP = "Tuvasta Sumovaljaku joon.";
